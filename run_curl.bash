65e26d2b9e4f1af3a3200e5467e96285ecce6f0f
Sanjay Ghosh <nerdsubhankar2006@gmail.com>
2023-03-11 21:38:33 +0530

    Add Fetch All Method

--- run_curl.bash
+++ run_curl.bash
@@ -1,3 +1,2 @@
-curl -d'{"lastName":"Parker"}' -H'Content-Type:application/json'  -k http://localhost:8080/employee/lastName
 curl -d'{"lastName":"Parker", "firstName":"Peter", "age":20, "email":"spiderman@web.com", "id":1}' -H'Content-Type:application/json'  -k http://localhost:8080/employee/save
 curl -k http://localhost:8080/employee/all
